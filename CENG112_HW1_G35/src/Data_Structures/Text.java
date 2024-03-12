320617ddb7d9440b6680c3a2047d9ef93e437d23
Mert98 <smilinggreenfrog31@gmail.com>
2024-03-12 10:28:44 +0300

    Quick Fix

--- CENG112_HW1_G35/src/Data_Structures/Text.java
+++ CENG112_HW1_G35/src/Data_Structures/Text.java
@@ -1,8 +1,11 @@
 package Data_Structures;
+import Entities.Word;
 
 public class Text<T> extends Bag<T> {
-	 public void separate(Dictionary<Word> dictionary, 
-			 RareWordsDictionary<Word> rareWordsDictionary, 
-			 FrequentWordsDictionary<Word> frequentWordsDictionary, 
-			 MostFrequentWordsDictionary<Word> mostFrequentWordsDictionary)
+    public void separate(Dictionary<Word> dictionary, 
+            RareWordsDictionary<Word> rareWordsDictionary, 
+            FrequentWordsDictionary<Word> frequentWordsDictionary, 
+            MostFrequentWordsDictionary<Word> mostFrequentWordsDictionary) {
+
+}
 }
--- CENG112_HW1_G35/src/Io/FileIO.java
+++ CENG112_HW1_G35/src/Io/FileIO.java
@@ -1,4 +1,7 @@
 package Io;
+import Data_Structures.Dictionary;
+import Data_Structures.Text;
+import Entities.Word;
 
 public class FileIO {
     public static void readFile(Text<Word> text, Dictionary<Word> dictionary) {
