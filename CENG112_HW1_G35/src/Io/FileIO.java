6a692e03d8a82a725cd1e34a6978e5cc542ccf1e
Mert98 <smilinggreenfrog31@gmail.com>
2024-03-17 02:52:54 +0300

    Update FileIO.java

--- CENG112_HW1_G35/src/Io/FileIO.java
+++ CENG112_HW1_G35/src/Io/FileIO.java
@@ -2,6 +2,7 @@ package Io;
 import Data_Structures.Dictionary;
 import Data_Structures.Text;
 import Entities.Word;
+import text.txt;
 
 public class FileIO {
     public static void readFile(Text<Word> text, Dictionary<Word> dictionary) {
