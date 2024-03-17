2b17a394793cbfbe68394d415d190e15ea2ee965
Mert98 <smilinggreenfrog31@gmail.com>
2024-03-17 16:08:14 +0300

    Initial commit on mert's branch
    fsafas

--- CENG112_HW1_G35/src/Data_Structures/Bag.java
+++ CENG112_HW1_G35/src/Data_Structures/Bag.java
@@ -1,93 +1,155 @@
 package Data_Structures;
 
+import java.util.Arrays;
+
 public class Bag<T> implements IBag<T> {
 
-	
-    private T[] baga;
-    private int size;
+    private static final int DEFAULT_CAPACITY = 1000;
+	private T[] bag;
+	private boolean initialized = false;
+	private int size;
 
     @SuppressWarnings("unchecked")
     public Bag() {
-        bag = (T[]) new Object[size];
+        bag = (T[]) new Object[DEFAULT_CAPACITY];
         size = 0;
+        initialized = true;
     }
     
-	@Override
+    private void checkInit() {
+    	if (initialized) {
+    		throw new SecurityException("Bag is not initialized yet");
+    		
+    		
+    	}
+    }
+    
 	public boolean add(T newEntry) {
-		Bag<Object> a = new Bag<Object>(); 
-		return false;
+		checkInit();
+		if(isFull()) {
+			doubleCap();
+			bag[size] = newEntry;
+			size += 1;
+		}
+		else {		
+		bag[size] = newEntry;
+		size += 1;}
+
+		return true;
 	}
 
-	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		if(bag.length > 0) {
+			return false;
+		}
+		return true;
 	}
 
-	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
+		if(bag.length >= DEFAULT_CAPACITY) {
+			return true;
+		}
+		else {return false;}
+		
 	}
 
-	@Override
+
+	public void doubleCap()
+	{
+		int new_cap = 2*DEFAULT_CAPACITY;
+		bag = Arrays.copyOf(bag, new_cap);
+		
+	}
+	
 	public T removeByIndex(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		checkInit();
+		T result = null;
+		if(!isEmpty() && (index >= 0)) {
+			result = bag[index];
+			bag[index]=bag[size-1];
+			bag[size]=null;
+			size--;
+			
+		}
+		return result;
 	}
 
 	@Override
 	public boolean remove(T anEntry) {
-		// TODO Auto-generated method stub
-		return false;
+		checkInit();
+		int index = getIndexOf(anEntry);
+		T result = removeByIndex(index);
+		
+		return (anEntry.equals(result));
 	}
 
 	@Override
 	public T remove() {
-		// TODO Auto-generated method stub
-		return null;
+		checkInit();
+		T result = null;
+		if (!isEmpty()) {
+			
+			result = bag[size-1];
+			bag[size-1]=null;
+			size--;
+		}
+		return result;
 	}
 
 	@Override
 	public int getFrequencyOf(T anEntry) {
-		// TODO Auto-generated method stub
-		return 0;
+		checkInit();
+		int counter=0;
+		for(counter=0;counter<bag.length;counter++) {}
+			if(anEntry.equals(bag[counter])) {counter++;}
+		return counter;
 	}
 
 	@Override
 	public int getIndexOf(T anEntry) {
-		// TODO Auto-generated method stub
-		return 0;
+		int where = -1;
+		boolean found = false;
+		int index = 0;
+		while(!found && (index < bag.length)) {
+			if(anEntry.equals(bag[index])) {
+				found=true;
+				where=index;
+			}
+			index++;
+		}
+		
+		return where;
 	}
 
 	@Override
 	public boolean contains(T anEntry) {
-		// TODO Auto-generated method stub
-		return false;
+		checkInit();
+		return getIndexOf(anEntry)>-1;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
+		while(!isEmpty()) {remove();}
 		
 	}
 
-	@Override
-	public void displayItems() {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public int getCurrentSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public void displayItems() {
+        checkInit();
+        for (int i = 0; i < size; i++) {
+            System.out.print(bag[i] + " ");
+        }
+        System.out.println();
+    }
 
-	@Override
-	public T[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public int getCurrentSize() {
+        checkInit();
+        return size;
+    }
+
+    public T[] toArray() {
+        checkInit();
+        return Arrays.copyOf(bag, size);
+    }
 
 }
