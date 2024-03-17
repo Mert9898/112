514ffde80d70699f9b72d6b313f825b8b906141d
Mert98 <smilinggreenfrog31@gmail.com>
2024-03-17 17:15:31 +0300

    Word and Counter implemented basically

--- CENG112_HW1_G35/src/Entities/Word.java
+++ CENG112_HW1_G35/src/Entities/Word.java
@@ -3,7 +3,36 @@ package Entities;
 public class Word {
     private String content;
     private int length;
-    public String toString();
-    public boolean equals(Object obj);
+    
+    public Word(String content) {
+        this.content = content;
+        this.length = content.length();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public String toString() {
+        return content;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Word)) {
+            return false;
+        }
+        Word otherWord = (Word) obj;
+        return this.content.equals(otherWord.content);
+    }
+}
     
 }
--- CENG112_HW1_G35/src/Main/WordFrequencyCounter.java
+++ CENG112_HW1_G35/src/Main/WordFrequencyCounter.java
@@ -1,7 +1,36 @@
 package Main;
+import Entities.Word;
+import Io.FileIO;
+import Data_Structures.FrequentWordsDictionary;
+import Data_Structures.Dictionary;
+import Data_Structures.Text;
+import Data_Structures.MostFrequentWordsDictionary;
+import Data_Structures.RareWordsDictionary;
 
 public class WordFrequencyCounter {
     public static void main(String[] args) {
-       
+        Text<Word> text = new Text<>();
+        Dictionary<Word> dictionary = new Dictionary<>();
+        RareWordsDictionary<Word> rareWordsDictionary = new RareWordsDictionary<>();
+        FrequentWordsDictionary<Word> frequentWordsDictionary = new FrequentWordsDictionary<>();
+        MostFrequentWordsDictionary<Word> mostFrequentWordsDictionary = new MostFrequentWordsDictionary<>();
+
+        FileIO.readFile(text, dictionary, "text.txt");
+        text.displayItems();
+        System.out.println("Text Size: " + text.getCurrentSize());
+
+        text.separate(dictionary, rareWordsDictionary, frequentWordsDictionary, mostFrequentWordsDictionary);
+
+        System.out.println("\nRare Words Dictionary:");
+        rareWordsDictionary.displayItems();
+        System.out.println("Size: " + rareWordsDictionary.getCurrentSize());
+
+        System.out.println("\nFrequent Words Dictionary:");
+        frequentWordsDictionary.displayItems();
+        System.out.println("Size: " + frequentWordsDictionary.getCurrentSize());
+
+        System.out.println("\nMost Frequent Words Dictionary:");
+        mostFrequentWordsDictionary.displayItems();
+        System.out.println("Size: " + mostFrequentWordsDictionary.getCurrentSize());
     }
 }
